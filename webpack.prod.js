98436be676d4587a88f3c5d1ef77b370dac679b3
super <zxpscau@163.com>
2018-09-21 18:16:15 +0800

    update

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,16 +7,16 @@ const common = require('./webpack.base.js');
 
 module.exports = merge(common, {
   // entry: './carousal-test.js', //入口
-  entry: './index.js', //入口
+  entry: './colorGame-dev.js', //入口
   module: {},
   plugins: [],
   mode: 'production',
   output: {
     path: path.resolve(__dirname, './'),
-    library: 'Carousal',
+    library: 'ColorGame',
     libraryExport: "default",
     libraryTarget: 'umd',
-    filename: 'alert.js',
+    filename: 'colorGame.js',
     // filename: 'carousal.js',
   },
 });
